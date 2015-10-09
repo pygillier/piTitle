816496435b1f290f355d6c33f06a7efd90e4f48a
pivwan <pivwan@gmail.com>
2015-10-09 07:59:51 -0400

    Sensio Insight fixes

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Imagine\Image\Box;
 use Neutron\Silex\Provider\ImagineServiceProvider;
 use Neutron\Silex\Provider\FilesystemServiceProvider;
